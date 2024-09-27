7695a0683c8dd64591e4d523418dc9214c4eff46
Paweł Wrzesiński <pj.wrzesinski34@gmail.com>
2024-09-27 14:55:02 +0200

    Zadanie sprawdzające palindromy

--- palindromy.py
+++ palindromy.py
@@ -1,4 +1,15 @@
-print("hello")
-print ("world")
-print("2+2")
-print("urodziłem się w 2000")
+def is_it_palindrome (word):
+    # Usunięcie zbędnych spacji oraz zamiana liter na małe, żeby nie zwracać uwagi na wielkość liter
+    word = word.replace(" ", "").lower()
+    
+    # Sprawdzenie, czy słowo jest takie samo jak jego odwrócona wersja
+    if word == word[::-1]:
+        return f'"{word}" jest palindromem.'
+    else:
+        return f'"{word}" nie jest palindromem.'
+
+# Pobranie słowa od użytkownika
+uzytkownik_input = input("Wpisz słowo do sprawdzenia, czy jest palindromem: ")
+# Wywołanie funkcji i wydrukowanie wyniku
+wynik = is_it_palindrome(uzytkownik_input)
+print(wynik)
